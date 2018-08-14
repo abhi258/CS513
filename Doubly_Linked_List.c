3f56b7a38c5380d30da275f99eb3c243cdba202f
Ayush Jaiswal <ayushjaiswal199@gmail.com>
2018-08-14 21:59:06 +0530

    Implemented append() and insert() functions

--- Doubly_Linked_List.c
+++ Doubly_Linked_List.c
@@ -13,11 +13,43 @@ int length=0;
 
 void append(int d, int flg){
 	//appends d to front end of the list if the flag is 0
-	//appends d to rear end of the list if the flag is 1 
+	//appends d to rear end of the list if the flag is 1
+	node* p = (node*)malloc(sizeof(node));
+	p->d = d;
+	if(flg==0){
+		p->right = head;
+		p->left = NULL;
+		head->left = p;
+		head = p;
+	}
+	else{
+		p->left = head;
+		p->right=NULL;
+		tail->right = p;
+		tail = p;
+	}
+
 }
 
 void insert(int d, int pos){
 	//inserts d to the pos-th position of the list
+	node* p = (node*)malloc(sizeof(node));
+	p->d = d;
+	int i = 0;
+	node* t = head;
+	node* q = NULL;
+	while(t->right!=NULL){
+		if(pos==i){
+			q->right = p;
+			p->left = q;
+			p->right = t;
+			t->left = p;
+			break;
+		}
+		q = t;
+		t = t->link;
+		i++;
+	}
 }
 
 void delete(int d){
@@ -37,6 +69,11 @@ void traverse(int flg){
 
 int main(){
 	int exit=1;
+	head = (node*)malloc(sizeof(node));
+	tail = head;
+	head->right=NULL;
+	head->left=NULL;
+
 	do{
 		int c;
 		printf("1. Append an Element\n2. Insert an Element\n3. Delete an Element\n4. Search for an Element\n5.Print the List\n6. Print the Listin reverse\n7. Exit");
@@ -48,7 +85,8 @@ int main(){
 				if(length==0){
 					printf("Enter element:\n");
 					scanf("%d",&d);
-					append(d,0);
+					head->d=d;
+					length++;
 				}
 				else{
 					int flg;
@@ -56,11 +94,33 @@ int main(){
 					scanf("%d",&d);
 					printf("Front(0) or Rear(1):\n");
 					scanf("%d",&flg);
+					length++;
 					append(d,flg);
 				}
 				break;
 			case 2:
-				insert(d,pos);
+				int d;
+				if(length==0){
+					printf("Enter element:\n");
+					scanf("%d",&d);
+					head->d=d;
+					length++;
+				}
+				else{
+					int pos;
+					printf("Enter element:\n");
+					scanf("%d",&d);
+					printf("Enter position to insert:\n");
+					scanf("%d",&pos);
+					if(pos<0){
+						printf("Invalid position\n");
+						break;
+					}
+					if(pos==0 || pos==length)
+						append(d,pos/(length-1));
+					else
+						insert(d,pos);
+				}
 				break;
 			case 3:
 				delete(d);
